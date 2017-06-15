43a151db809251c4c1d331cf6be2043eaa96add2
Luis Gonzalez Espejo <luis.gonzalez.espejo@gmail.com>
2017-06-15 20:50:57 -0300

    cambios de activity

--- app/src/main/java/com/learnandroid/MainActivity.java
+++ app/src/main/java/com/learnandroid/MainActivity.java
@@ -12,5 +12,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle(ApplicationName.APPLICATION_NAME);
+
+        //called when the activity is first created
     }
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        //called when the activity is becoming visible
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        //called when the activity will start interacting with the user
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        //called when an activity is being paused
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        //called when the activity is being stopped and is no loger visible to the user
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        //called when an activity being destroyed
+
+    }
+
+
+
+
+
+
 }
